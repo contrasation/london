3b56b6e8eb88f2469466f94f4d201a0d0c5c5ebe
hanabata <contrasation805@gmail.com>
2017-03-21 22:43:52 +0900

    2017/03/21
    3rd commit

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -18,6 +18,7 @@ class Genre(models.Model):
         """
         return self.name
 
+
 class Language(models.Model):
     """
     Model representing a Language (e.g. English, French, Japanese, etc.)
@@ -108,7 +109,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    data_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
 
     def get_absolute_url(self):
         """
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 # Create your views here.
 
-def top(request):
-    return HttpResponse('Catalog!')
+def index(request):
+
+    """
+    View function for home page of site.
+    :param request:
+    :return:
+    """
+    # Generate counts of some of the main objects.
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status_exact='a').count()
+    num_authors = Author.objects.count() # The 'all()' is implied by default.
+
+    # Render the HTML template index.html with the data in the context variable.
+    return render(
+        request,
+        'index.html',
+        countext = {'num_books': num_books, 'num_instances': num_instances,
+                    'num_instances_availabel': num_instances_available,
+                    'num_authors': num_authors},
+    )
